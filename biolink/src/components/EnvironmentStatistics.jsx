4e8b29f7b9eb788c390e9904703c92090cc27756
DrWizardDrunkenstein54 <48422595+DrWizardDrunkenstein54@users.noreply.github.com>
2022-06-29 00:04:28 -0700

    Problems and Charts
    2/3 charts and problems explained

---
+++ biolink/src/components/EnvironmentStatistics.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+function EnvironmentStatistics() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default EnvironmentStatistics;
--- biolink/src/components/ProblemStatement.jsx
+++ biolink/src/components/ProblemStatement.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
+import MainProblem from './MainProblem';
+import ProblemStatistics from './ProblemStatistics';
+import EnvironmentProblem from './EnvironmentProblem';
+import EnvironmentStatistics from './EnvironmentStatistics';
 
 function ProblemStatement() {
     return (
-        <div id="problem-statement" className="colored-section">
-            
+        <div id="problem-statement" className="colored-section d-flex justify-content-around flex-wrap w-100 p-3 my-5">
+            <MainProblem />
+            <ProblemStatistics />
+            <EnvironmentProblem />
+            <EnvironmentStatistics />
         </div>
     );
 }
